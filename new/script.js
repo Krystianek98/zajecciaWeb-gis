15ec256c62e2303619c5a2f9f4de395a3ec484b2
Krystianek98 <59928320+Krystianek98@users.noreply.github.com>
2020-01-22 18:27:45 +0100

    Add files via upload

--- new/script.js
+++ new/script.js
@@ -1,12 +1,34 @@
 
-require (["esri/Map", "esri/views/MapView"],
-   function(Map, MapView)
-   {
-    let map1 = new Map({basemap:"topo"}); //słowo klucz new + nazwa modułu
-
-    let mapContainer = new MapView({
-        container: "map",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
-        map: map1,  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
-        zoom: 12
+require (["esri/Map", "esri/views/MapView", "esri/WebMap"],
+function(Map, MapView, WebMap)
+{ 
+    let map1 = new Map({basemap:"topo"});
+    let map2 = new Map({basemap:"satellite"});
+    let map3 = new Map({basemap:"osm"});
+    let map4 = new WebMap({portalItem: {
+            id: "e435d8b878604fd2aa4d62b1020fe9a7"
+        }
      });
-   })
+
+ let mapContainer = new MapView({
+     container: "mapid",  
+     map: map4,  
+     
+  });
+  document.getElementById("przycisk1").onclick = function(){
+      console.log('klik')
+      mapContainer.map = map1;
+  }
+  document.getElementById("przycisk2").onclick = function(){
+    console.log('klik')
+    mapContainer.map = map2;
+  }
+  document.getElementById("przycisk3").onclick = function(){
+    console.log('klik')
+    mapContainer.map = map3;
+  }
+  document.getElementById("przycisk4").onclick = function(){
+    console.log('klik')
+    mapContainer.map = map4;
+  } 
+})
